78ff59e68de128c20243cf4243af930498aca4cd
jagdeepjain <jagdeep.jain@gmail.com>
2017-02-08 16:26:30 +0530

    remove not required code in UsersCreateServiceTest

--- src/test/java/io/dchq/sdk/core/users/UsersCreateServiceTest.java
+++ src/test/java/io/dchq/sdk/core/users/UsersCreateServiceTest.java
@@ -168,15 +168,6 @@ public class UsersCreateServiceTest extends AbstractServiceTest {
         }
     }
     
-    public static Profile getProfile(String name) {
-        try {
-            ProfileSearchServiceTest tempProf = new ProfileSearchServiceTest();
-            return tempProf.searchProfile(name);
-        } catch (Exception e) {
-            System.out.println("ERROR :" + e.getMessage());
-        }
-        return null;
-    }
 
     public static Organization getOrganization(String name, Boolean inActive, Boolean deleted) {
         return new Organization().withName(name).withInactive(inActive).withDeleted(deleted);
