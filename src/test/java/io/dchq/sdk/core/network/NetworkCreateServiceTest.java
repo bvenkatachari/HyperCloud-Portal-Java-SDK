1658bda9f8d1d5b016a986eb69925a4db0cfebd0
jjain-msys <jagdeep.jain@msystechnologies.com>
2017-02-28 16:14:01 +0530

    added test parameter for create network test case

--- src/test/java/io/dchq/sdk/core/network/NetworkCreateServiceTest.java
+++ src/test/java/io/dchq/sdk/core/network/NetworkCreateServiceTest.java
@@ -2,6 +2,11 @@ package io.dchq.sdk.core.network;
 
 import static junit.framework.TestCase.assertNotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
@@ -11,6 +16,7 @@ import org.junit.runners.Parameterized;
 import com.dchq.schema.beans.base.Message;
 import com.dchq.schema.beans.base.ResponseEntity;
 import com.dchq.schema.beans.host.Network;
+import com.dchq.schema.beans.one.base.UsernameEntityBase;
 import com.dchq.schema.beans.one.dockervolume.DockerVolume;
 import com.dchq.schema.beans.one.network.DockerNetwork;
 
@@ -57,7 +63,19 @@ public class NetworkCreateServiceTest extends AbstractServiceTest{
     	network.setDriver(driver);
     	network.setDockerServerName(server);
     }
-    
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() throws Exception {
+ 	   List<UsernameEntityBase> entitledUser = new ArrayList<>();
+ 	   UsernameEntityBase user = new UsernameEntityBase();
+ 	   user.setUsername("Everyone");
+ 	   entitledUser.add(user);
+        return Arrays.asList(new Object[][]{
+                // TODO: add more test data for all sorts of validations
+ 				{ "testnetwork", "bridge",entitledUser,"qa-100(DockerEngine))"},
+ 				{ "test@@network", "bridge",entitledUser,"qa-100(DockerEngine))"},
+ 				{ "test-network", "bridge",entitledUser,"qa-100(DockerEngine))"}
+        });
+    }
     @Ignore
     public void createTest()
     {
