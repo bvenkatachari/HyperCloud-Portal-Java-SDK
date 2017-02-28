11157f2f07947649b83c7716755f44a9b2e266b0
jjain-msys <jagdeep.jain@msystechnologies.com>
2017-02-28 16:09:32 +0530

    added test cases for create volumne

--- src/test/java/io/dchq/volumes/DockerVolumeCreateServiceTest.java
+++ src/test/java/io/dchq/volumes/DockerVolumeCreateServiceTest.java
@@ -1,16 +1,27 @@
 package io.dchq.volumes;
 
-import com.dchq.schema.beans.base.ResponseEntity;
-import com.dchq.schema.beans.one.dockervolume.DockerVolume;
-import io.dchq.sdk.core.AbstractServiceTest;
-import io.dchq.sdk.core.DockerVolumeService;
-import io.dchq.sdk.core.ServiceFactory;
+import static junit.framework.TestCase.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.junit.runners.Parameterized;
+
+import com.dchq.schema.beans.base.Message;
+import com.dchq.schema.beans.base.ResponseEntity;
+import com.dchq.schema.beans.one.base.UsernameEntityBase;
+import com.dchq.schema.beans.one.dockervolume.DockerVolume;
+
+import io.dchq.sdk.core.AbstractServiceTest;
+import io.dchq.sdk.core.DockerVolumeService;
+import io.dchq.sdk.core.ServiceFactory;
 
 /**
  * @Author Saurabh Bhatia
@@ -19,7 +30,6 @@ import org.junit.runners.Parameterized;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(Parameterized.class)
-@Ignore
 public class DockerVolumeCreateServiceTest extends AbstractServiceTest{
 
     private DockerVolumeService dockerVolumeService;
@@ -29,15 +39,20 @@ public class DockerVolumeCreateServiceTest extends AbstractServiceTest{
     boolean error;
     String validationMessage;
 
-@Ignore
     @org.junit.Before
     public void setUp() throws Exception{
         dockerVolumeService = ServiceFactory.buildDockerVolumeService(rootUrl, username, password);
     }
 
-   public DockerVolumeCreateServiceTest (String volumeName) {
+   public DockerVolumeCreateServiceTest (
+		   String volumeName,
+			String options, 
+			List<UsernameEntityBase> entitledUser, 
+			String provider, 
+			String server,
+			String message,
+			boolean error) {
 
-       // random clustername
        if (volumeName == null) {
            throw new IllegalArgumentException("Volume == null");
        }
@@ -50,5 +65,36 @@ public class DockerVolumeCreateServiceTest extends AbstractServiceTest{
 
        this.dockerVolume = new DockerVolume();
        this.dockerVolume.setName(volumeName);
+       this.dockerVolume.setOptionsText(options);
+       this.dockerVolume.setEntitledUsers(entitledUser);
+   }
+   @Parameterized.Parameters
+   public static Collection<Object[]> data() throws Exception {
+	   List<UsernameEntityBase> entitledUser = new ArrayList<>();
+	   UsernameEntityBase user = new UsernameEntityBase();
+	   user.setUsername("Everyone");
+	   entitledUser.add(user);
+       return Arrays.asList(new Object[][]{
+               // TODO: add more test data for all sorts of validations
+				{ "testvalumn", "test options",entitledUser, "Local Volumne Provider" + "qe-100(DockerEngine)"},
+				{ "test@123", "test @@@ options",entitledUser, "Local Volumne Provider" + "qe-100(DockerEngine)"},
+				{ "test-valumn", "test options",entitledUser, "Local Volumne Provider" + "qe-100(DockerEngine)"}
+       });
+   }
+   @Ignore
+   public void testCreate()
+   {
+	   logger.info("Create docker volumne name[{}] options [{}] EntitledUser [{}] ", dockerVolume.getName(), dockerVolume.getOptionsText(), dockerVolume.getEntitledUsers().get(0));
+       ResponseEntity<DockerVolume> response = dockerVolumeService.create(dockerVolume);
+       for (Message message : response.getMessages()) {
+           logger.warn("Error while Create request  [{}] ", message.getMessageText());
+       }
+       if (response.getResults() != null) {
+           this.dockerVolumeCreated = response.getResults();
+           logger.info("Create docker volumne Successful..");
+       }
+       assertNotNull(response);
+       assertNotNull(response.isErrors());
+       
    }
 }
