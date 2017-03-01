c2fab94bbf322ea927826514163ed52890dc787a
jjain-msys <jagdeep.jain@msystechnologies.com>
2017-03-01 18:09:16 +0530

    updated test case for docker network

--- src/test/java/io/dchq/sdk/core/network/NetworkCreateServiceTest.java
+++ src/test/java/io/dchq/sdk/core/network/NetworkCreateServiceTest.java
@@ -1,23 +1,22 @@
 package io.dchq.sdk.core.network;
 
 import static junit.framework.TestCase.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.junit.runners.Parameterized;
 
 import com.dchq.schema.beans.base.Message;
 import com.dchq.schema.beans.base.ResponseEntity;
-import com.dchq.schema.beans.host.Network;
-import com.dchq.schema.beans.one.base.UsernameEntityBase;
-import com.dchq.schema.beans.one.dockervolume.DockerVolume;
 import com.dchq.schema.beans.one.network.DockerNetwork;
 
 import io.dchq.sdk.core.AbstractServiceTest;
@@ -37,10 +36,9 @@ public class NetworkCreateServiceTest extends AbstractServiceTest{
 
     private NetworkService networkService;
 
-@Ignore
     @org.junit.Before
     public void setUp() throws Exception{
-        networkService = ServiceFactory.buildNetworkService(rootUrl, username, password);
+        networkService = ServiceFactory.buildNetworkService(rootUrl, cloudadminusername, cloudadminpassword);
     }
 
     DockerNetwork network;
@@ -52,44 +50,56 @@ public class NetworkCreateServiceTest extends AbstractServiceTest{
     public NetworkCreateServiceTest (
     		String name,
     		String driver,
-    		String entitledUser,
     		String server
     		) {
-
-    	if (network == null) {
-            throw new IllegalArgumentException("Volume == null");
-        }
+    	network = new DockerNetwork();
     	network.setName(name);
     	network.setDriver(driver);
     	network.setDockerServerName(server);
     }
     @Parameterized.Parameters
     public static Collection<Object[]> data() throws Exception {
- 	   List<UsernameEntityBase> entitledUser = new ArrayList<>();
- 	   UsernameEntityBase user = new UsernameEntityBase();
- 	   user.setUsername("Everyone");
- 	   entitledUser.add(user);
         return Arrays.asList(new Object[][]{
-                // TODO: add more test data for all sorts of validations
- 				{ "testnetwork", "bridge",entitledUser,"qa-100(DockerEngine))"},
- 				{ "test@@network", "bridge",entitledUser,"qa-100(DockerEngine))"},
- 				{ "test-network", "bridge",entitledUser,"qa-100(DockerEngine))"}
+ 				{ "testnetwork", "bridge","qa-100(DockerEngine))"}
         });
     }
+  
     @Ignore
-    public void createTest()
-    {
-    	logger.info("Create network name[{}] driver [{}] server [{}]", network.getName(), network.getDriver(), network.getDockerServerName());
-        ResponseEntity<DockerNetwork> response = networkService.create(network);
-        for (Message message : response.getMessages()) {
-            logger.warn("Error while Create request  [{}] ", message.getMessageText());
-        }
-        if (response.getResults() != null) {
-            this.networkCreated = response.getResults();
-            logger.info("Create docker network Successful..");
-        }
-        assertNotNull(response);
-        assertNotNull(response.isErrors());
-    }
+	public void createTest() {
 
+		logger.info("Create network name[{}] driver [{}] server [{}]", network.getName(), network.getDriver(),
+				network.getDockerServerName());
+		ResponseEntity<DockerNetwork> response = networkService.create(network);
+		
+		for (Message message : response.getMessages()) {
+			logger.warn("Error while Create request  [{}] ", message.getMessageText());
+		}
+		Assert.assertFalse(response.isErrors());
+
+		if (response.getResults() != null && !response.isErrors()) {
+
+			this.networkCreated = response.getResults();
+			logger.info("Create docker network Successful..");
+		}
+		assertNotNull(response);
+		assertNotNull(response.isErrors());
+		if (this.networkCreated != null) {
+			assertNotNull(response.getResults().getId());
+			assertNotNull(networkCreated.getId());
+			assertEquals(network.getName(), networkCreated.getName());
+			assertEquals(network.getDriver(), networkCreated.getDriver());
+			assertEquals(network.getDockerServerName(), networkCreated.getDockerServerName());
+		}
+	}
+    
+    @After
+    public void cleanUp() {
+        if (this.networkCreated != null) {
+			logger.info("cleaning up...");
+			ResponseEntity<?> response = networkService.delete(this.networkCreated.getId());
+			for (Message message : response.getMessages()) {
+				logger.warn("Error network deletion: [{}] ", message.getMessageText());
+			}
+		}
+    }
 }
