abc49a30157784c48c634e5e5d1ea23ac246f50b
jjain-msys <jagdeep.jain@msystechnologies.com>
2017-05-09 17:07:55 +0530

    added more test cases for Create User

--- src/test/java/io/dchq/sdk/core/users/UsersCreateServiceTest.java
+++ src/test/java/io/dchq/sdk/core/users/UsersCreateServiceTest.java
@@ -21,6 +21,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -29,7 +30,6 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
@@ -65,7 +65,7 @@ public class UsersCreateServiceTest extends AbstractServiceTest {
     private boolean success;
     private Users userCreated;
     private String errorMessage;
-
+    
     public UsersCreateServiceTest(
             String fn,
             String ln,
@@ -80,19 +80,22 @@ public class UsersCreateServiceTest extends AbstractServiceTest {
             String pass,
             Boolean isActive,
             String message,
-            boolean success
+            boolean success,
+            boolean isUsernamePrefix,
+            boolean isEmailIdPrefix
     ) {
         // random user name
         String prefix = RandomStringUtils.randomAlphabetic(3);
-        if(username!=null && !username.isEmpty())
+        if(isUsernamePrefix)
         {
         	username = prefix + "-" + username;
         }
-        if(email !=null && !email.isEmpty())
+        if(isEmailIdPrefix)
         {
         	email = prefix + "-" + email;
         }
         // lower case
+        
         username = org.apache.commons.lang3.StringUtils.lowerCase(username);
         email = org.apache.commons.lang3.StringUtils.lowerCase(email);
         this.users = new Users().withFirstname(fn).withLastname(ln).withUsername(username).withEmail(email).withPassword(pass);
@@ -109,77 +112,100 @@ public class UsersCreateServiceTest extends AbstractServiceTest {
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() throws Exception {
+    	
+    	List<String> authorities = new ArrayList<String>();
+		authorities.add("ROLE_ORG_ADMIN");
+		
         return Arrays.asList(new Object[][]{
 
-                { "fname", "lname", "user", "user" + "@dchq.io", "ABC", "Engg", "123-1231-121", null, false, null,
-						"pass1234", true, "comments", false },
-				{ "Rahul", "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, null,
-						"pass", true, "NoComments", false },
+               	{ "fname", "lname", "user", "user" + "@dchq.io", "ABC", "Engg", "123-1231-121", null, false, authorities, "pass1234", true, "comments", false , true, true },
+        	
+			  	{ "Rahul", "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "passwqeqweq", true, "NoComments", false , true, true },
                 // TODO: This is a BUG
-				//{ "  ", "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, null,
-						//"pass", true, "NoComments", true },
-				//{ "1234", "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, null,
-				//		"pass", true, "NoComments", false },
+				//{ "  ", "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234", true, "NoComments", true , true, true },
+			  	
+				//{ "1234", "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234", true, "NoComments", false , true, true },
                 // TODO: This is a BUG
-                //{ null, "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, null,
-						//"pass", true, "NoComments", true },
-				//{ "Rahul", "  ", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, null, "pass",
-				//		true, "NoComments", false },
-				//{ "Rahul", "Khanna", "   ", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, null, "pass",
-					//	true, "NoComments", false },
-				//{ "Rahul", "Khanna", "RahulKhanna", null, "BMW", "ENGG", "9848098480", null, false, null, "pass", true,
-					//	"NoComments", false },
-				//{ "Rahul", "Khanna", "RahulKhanna", "1234@123.com", "BMW", "ENGG", "9848098480", null, false, null,
-				//		"pass", true, "NoComments", false },
-				//{ "Rahul", "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "FINANCE", "asdfgh", null, false, null,
-					//	"pass", true, "NoComments", false },
-				//{ "Raj", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, null,
-				//		"1234", true, "NoComments", false },
-				//{ "Raj", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "@@@@@@@", null, false, null,
-				//		"fail", true, "  ", false },
-				//{ "@@@@", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "@@@@@@@", null, true, null,
-				//		"pass", true, "Comments", true },
+                //{ null, "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234", true, "NoComments", true , true, true },
+				//{ "Rahul", "  ", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234", true, "NoComments", false , true, true },
+				//{ "Rahul", "Khanna", "   ", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234", true, "NoComments", false , false, true },
+				//{ "Rahul", "Khanna", "RahulKhanna", null, "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234", true, "NoComments", false , true, false },
+				//{ "Rahul", "Khanna", "RahulKhanna", "1234@123.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234", true, "NoComments", false , true, true },
+				//{ "Rahul", "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "FINANCE", "asdfgh", null, false, authorities, "pass1234", true, "NoComments", false , true, true },
+				//{ "Raj", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "1234", true, "NoComments", false , true, true },
+				//{ "Raj", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "@@@@@@@", null, false, authorities, "fail", true, "  ", false , true, true },
+				//{ "@@@@", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "@@@@@@@", null, true, authorities,	"pass1234", true, "Comments", true , true, true },
+			  	
 				//TODO failing 
-				//{ "@@@@", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "@@@@@@@", null, true, null,
-				//		"pass", false, "Comments", true },
-				//{ "@@@@", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "@@@@@@@", null, true, null,
-				//		"pass", true, "Comments", true },
+				//{ "@@@@", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "@@@@@@@", null, true, authorities,	"pass1234", false, "Comments", true , true, true },
+				//{ "@@@@", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "@@@@@@@", null, true, authorities,	"pass1234", true, "Comments", true , true, true },
                 // TODO: This is a failing
-                //{ "@@@@", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "@@@@@@@", null, false, null,
-                //"fail", true, "Comments", true },
-
-                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
-                //{ "Hyper_Hyper", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
-                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "     ", "QA", "ABC", null, false, null,  "pass", true, "     ", false },
-                //{ "12345", "&&&&", "hyperuser", "user@hyperuser.com", null, "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
-                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, true, null,   "pass", true, "Comments", false },
-                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, true, null,  "fail", true, null, false },
-                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "12345", "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
-                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "0000000", "9999999", null, false, null, "pass", true, "Comments", false },
-               // { "Hyper", null, null, null, "Hyper User", "QA", "9999999", null, false, null, "pass", true, "Comments", false },
-                //{ "Hyper1234", "    ", "&&&&", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
+                //{ "@@@@", "Malhotra", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "@@@@@@@", null, false, authorities, "fail", true, "Comments", true , true, true },
+                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", false , true, true },
+                //{ "Hyper_Hyper", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", false , true, true },
+                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "     ", "QA", "ABC", null, false, authorities,  "pass1234", true, "     ", false , true, true },
+                //{ "12345", "&&&&", "hyperuser", "user@hyperuser.com", null, "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", false , true, true },
+                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, true, authorities,   "pass1234", true, "Comments", false , true, true },
+                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, true, authorities,  "fail", true, null, false , true, true },
+                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "12345", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", false , true, true },
+                //{ "Hyper", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "0000000", "9999999", null, false, authorities, "pass1234", true, "Comments", false , true, true },
+                //{ "Hyper", null, null, null, "Hyper User", "QA", "9999999", null, false, authorities, "pass1234", true, "Comments", false , false, false },
+                //{ "Hyper1234", "    ", "&&&&", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", false , false, true },
                 // TODO: This is a BUG 
-                //{ "  ", "User", "12345", null, "Hyper User", "QA", "9999999", null, false, null,   "pass", true, "Comments", true },
-             // TODO: This is a BUG
-               // { "#####", "2020", "    ", "user@hyperuser.com", "Hyper User", "100", "XYZ", null, false, null,   "pass", true, "Comments", true },
-                //{ "Hyper", "User", "hyperuser", "00000", null, "QA", "9999999", null, false, null,   "pass", true, "    ", false },
-                //{ "123_123Hyper", "User-New", null, "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
-             // TODO: This is a BUG
-                //{ " ", null, "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, null, "pass", true, "Comments", true },
-             // TODO: This is a BUG
-                //{ null, null, null, "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, null,   "pass", true, "Comments", true },
-             // TODO: This is a BUG
-                //{ " ", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", null, null, false, null,   "pass", true, "Comments", true },
-                //{ "Hyper", "User", " ", " ", "Hyper User", "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
-                //{ "Hyper", "  ", "hyperuser", " ", "Hyper User", "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
-             // TODO: This is a BUG
-                //{ " ", "User", "hyperuser", "user@hyperuser.com", " ", "QA", "9999999", null, false, null,   "pass", true, "Comments", true },
-             // TODO: This is a BUG
-                //{ " ", "User", "hyperuser", "user@hyperuser.com", "Hyper User", " ", "9999999", null, false, null,   "pass", true, "Comments", true },
-               // { "12345", "12345", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
-               // { "12345", "12345", "hyperuser", "user@hyperuser.com", "12345", "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
-               // { "12345", "12345", "12345", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, null,   "pass", true, "Comments", false },
-
+                //{ "  ", "User", "12345", null, "Hyper User", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", true , true, true },
+			  	// TODO: This is a BUG
+			  	// { "#####", "2020", "    ", "user@hyperuser.com", "Hyper User", "100", "XYZ", null, false, authorities,   "pass1234", true, "Comments", true , true, true },
+                //{ "Hyper", "User", "hyperuser", "00000", null, "QA", "9999999", null, false, authorities,   "pass1234", true, "    ", false , true, true },
+                //{ "123_123Hyper", "User-New", null, "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", false , false, true },
+			  	// TODO: This is a BUG
+                //{ " ", null, "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, authorities, "pass1234", true, "Comments", true , true, true },
+			  	// TODO: This is a BUG
+                //{ null, null, null, "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", true , false, true },
+			  	// TODO: This is a BUG
+                //{ " ", "User", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", null, null, false, authorities,   "pass1234", true, "Comments", true , true, true },
+                //{ "Hyper", "User", " ", " ", "Hyper User", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", true , false, false },
+                //{ "Hyper", "  ", "hyperuser", " ", "Hyper User", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", true , true, true },
+			  	// TODO: This is a BUG
+                //{ " ", "User", "hyperuser", "user@hyperuser.com", " ", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", true , true, true },
+			  	// TODO: This is a BUG
+                //{ " ", "User", "hyperuser", "user@hyperuser.com", "Hyper User", " ", "9999999", null, false, authorities,   "pass1234", true, "Comments", true , true, true },
+                //{ "12345", "12345", "hyperuser", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", false , true, true },
+                //{ "12345", "12345", "hyperuser", "user@hyperuser.com", "12345", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", false , true, true },
+                //{ "12345", "12345", "12345", "user@hyperuser.com", "Hyper User", "QA", "9999999", null, false, authorities,   "pass1234", true, "Comments", false , true, true },
+				
+				// TODO first name should have limit of length
+				//{ "Rahulsdasdasdasdadasdasdasdadsadadasdadadasdasdasdasdadasdasdasdasdasdasdasdassadasddddddddddaaaaaassssssssssssssssssssssssssssssssssssssssssssssssssssss"
+				//		+ "b", "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234", true, "NoComments", true , true, true },
+        	    // TODO last name should have limit of length
+				//{ "test ", "Rahulsdasdasdasdadasdasdasdadsadadasdadadasdasdasdasdadasdasdasdasdasdasdasdassadasddddddddddaaaaaassssssssssssssssssssssssssssssssssssssssssssssssssssss"
+					//		, "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234", true, "NoComments", true , true, true },
+        	     // TODO username should have limit of length
+				//{ "test ", "Khanna", "Rahulsdasdasdasdadasdasdasdadsadadasdadadasdasdasdasdadasdasdasdasdasdasdasdassadasddddddddddaaaaaassssssssssssssssssssssssssssssssssssssssssssssssssssss"
+				//		+ "", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234", true, "NoComments", true , true, true },
+				
+				//TODO duplicate username testing
+				//{ "Rahul", "Khanna", "Rahul", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass1234567", true, "NoComments", true , false, true },
+				
+				// duplicate emailId testing
+				{ "Rahul", "Khanna", "RahulKhanna", "rahul@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "pass212345", true, "NoComments", true , true, false },
+				
+				// TODO password length should not be less then 8 
+				//{ "Rahul", "Khanna", "RahulKhanna", "rahulddd@bmw.com", "BMW", "ENGG", "9848098480", null, false, authorities, "passwww222", true, "NoComments", true , true, true }
+				
+			  	// TODO Authorities(Roles) is mandatory field, it should not be null 
+				//{ "Rahul", "Khanna", "RahulKhanna", "rahulkhanna21@bmw.com", "BMW", "ENGG", "9848098480", null, false, null, "pass123456", true, "NoComments", true , true, true },
+				
+				// TODO Company name should not allow only special characters
+				//{ "Rahul", "Khanna", "RahulKhanna", "rahulkhanna21@bmw.com", "@@@@@", "ENGG", "9848098480", null, false, authorities, "pass123456", true, "NoComments", true , true, true },
+				//{ "Rahul", "Khanna", "RahulKhanna", "rahulkhanna21@bmw.com", "---+++++==", "ENGG", "9848098480", null, false, authorities, "pass123456", true, "NoComments", true , true, true },
+				
+				// TODO Title name should not be allow only special characters
+				//{ "Rahul", "Khanna", "RahulKhanna", "rahulkhanna21@bmw.com", "maruti", "++++444444444", "9848098480", null, false, authorities, "pass123456", true, "NoComments", true , true, true },
+				
+				// TODO phone number should have valid length
+				//{ "Rahul", "Khanna", "RahulKhanna", "rahulkhanna21@bmw.com", "maruti", "ENGG", "984", null, false, authorities, "pass123456", true, "NoComments", true , true, true },
+				// TODO Phone number should accept only numeric values
+				//{ "Rahul", "Khanna", "RahulKhanna", "rahulkhanna21@bmw.com", "maruti", "ENGG", "@@@@@@", null, false, authorities, "pass123456", true, "NoComments", true , true, true },
 
         });
         }
