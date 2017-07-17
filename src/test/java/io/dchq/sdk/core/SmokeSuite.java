ac65d02ab43c29a956be45db47b7319eae2b8e62
jjain-msys <jagdeep.jain@msystechnologies.com>
2017-07-17 13:09:53 +0530

    remove import and added fully qualified class name in test suite

--- src/test/java/io/dchq/sdk/core/SmokeSuite.java
+++ src/test/java/io/dchq/sdk/core/SmokeSuite.java
@@ -3,14 +3,10 @@ package io.dchq.sdk.core;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import io.dchq.sdk.core.smoke.testsuite.VPCCreateServiceTest;
-import io.dchq.sdk.core.smoke.testsuite.VPCUpdateServiceTest;
-
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	VPCCreateServiceTest.class, 
-	VPCUpdateServiceTest.class})
+	io.dchq.sdk.core.smoke.testsuite.VPCCreateServiceTest.class, 
+	io.dchq.sdk.core.smoke.testsuite.VPCUpdateServiceTest.class})
 public class SmokeSuite {
 
 }
