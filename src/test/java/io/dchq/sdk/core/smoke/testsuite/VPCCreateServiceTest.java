bd0bff9120f7feabce5a3a8a709c7323917341d7
jjain-msys <jagdeep.jain@msystechnologies.com>
2017-07-17 12:28:01 +0530

    Added create and update VPC for testing test suite

---
+++ src/test/java/io/dchq/sdk/core/smoke/testsuite/VPCCreateServiceTest.java
@@ -0,0 +1,132 @@
+package io.dchq.sdk.core.smoke.testsuite;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+
+import com.dchq.schema.beans.base.Message;
+import com.dchq.schema.beans.base.ResponseEntity;
+import com.dchq.schema.beans.one.base.NameEntityBase;
+import com.dchq.schema.beans.one.security.EntitlementType;
+import com.dchq.schema.beans.one.vpc.VirtualPrivateCloud;
+
+import io.dchq.sdk.core.AbstractServiceTest;
+import io.dchq.sdk.core.ServiceFactory;
+import io.dchq.sdk.core.VPCService;
+
+/**
+ * 
+ * @author Jagdeep Jain
+ */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(Parameterized.class)
+public class VPCCreateServiceTest extends AbstractServiceTest {
+	
+	private VPCService vpcService;
+	
+	VirtualPrivateCloud createVPC;
+	VirtualPrivateCloud createdVPC;
+	boolean sussess;
+	long startTime = System.currentTimeMillis();
+	long endTime = startTime + (60 * 60 * 90); // this is for 3 mints
+	
+	public VPCCreateServiceTest(String vpcName, String providerId, EntitlementType entitlementType, String ipv4Cidr, String description, boolean isprifix, boolean success)
+	{
+		String prifix = RandomStringUtils.randomAlphabetic(3);
+
+		if (vpcName != null && !vpcName.isEmpty() && isprifix) {
+			vpcName = (vpcName + prifix).toLowerCase();
+		}
+		createVPC = new VirtualPrivateCloud();
+		createVPC.setName(vpcName);
+		createVPC.setEntitlementType(entitlementType);
+		createVPC.setIpv4Cidr(ipv4Cidr);
+		NameEntityBase entity = new NameEntityBase();
+		entity.withId(providerId);
+		createVPC.setProvider(entity);
+		createVPC.setDescription(description);
+		this.sussess = success;
+
+	}
+	@Before
+	public void setUp()
+	{
+		vpcService = ServiceFactory.buildVPCService(rootUrl1, cloudadminusername, cloudadminpassword);
+	}
+	@Parameterized.Parameters
+	public static Collection<Object[]> data() throws Exception {
+		// provider id "8a818a105c83f42a015c83fd71240014" Intesar's machine
+		return Arrays.asList(new Object[][]{ 
+			{"testvpc", "2c9180865d312fc4015d314da1ca006a", EntitlementType.OWNER, "10.0.0.0/24", "descriptions test" , true, true},
+		});
+	}
+
+	@Test
+	public void createTest()
+	{
+		logger.info("Create VPC name[{}] ", createVPC.getName());
+		ResponseEntity<VirtualPrivateCloud> resultResponse = vpcService.create(createVPC);
+		Assert.assertNotNull(resultResponse);
+
+		for (Message msg : resultResponse.getMessages()) {
+			logger.warn("Error [{}]  " + msg.getMessageText());
+		}
+		if (this.sussess) {
+			Assert.assertEquals(false, resultResponse.isErrors());
+			Assert.assertNotNull(resultResponse.getResults());
+
+			if (resultResponse.getResults() != null && !resultResponse.isErrors()) {
+				this.createdVPC = resultResponse.getResults();
+				logger.info("Create VPC Successfully..");
+			}
+			logger.info("VPC state [{}]", createdVPC.getState().name());
+			while(createdVPC.getState().name().equals("PROVISIONING") && (System.currentTimeMillis() < endTime))
+			{
+				try {
+					// Sleep for some time
+					Thread.sleep(10000);
+					logger.info("VPC state[{}]", createdVPC.getState().name());
+					resultResponse = vpcService.findById(createdVPC.getId());
+					Assert.assertEquals(false, resultResponse.isErrors());
+					Assert.assertNotNull(resultResponse.getResults());
+					this.createdVPC = resultResponse.getResults();
+				} catch (InterruptedException e) {
+					// ignore
+				}
+			}
+			logger.info("VPC state [{}]", createdVPC.getState().name());
+			Assert.assertEquals("LIVE", createdVPC.getState().name());
+			Assert.assertEquals(createdVPC.getName(), createVPC.getName());
+			Assert.assertEquals(createdVPC.getProvider().getId(), createVPC.getProvider().getId());
+			Assert.assertEquals(createdVPC.getIpv4Cidr(), createVPC.getIpv4Cidr());
+
+		} else {
+
+			Assert.assertEquals(true, resultResponse.isErrors());
+			Assert.assertEquals(null, resultResponse.getResults());
+		}
+	}
+	@After
+	public void cleanUp()
+	{
+		if(this.createdVPC !=null)
+		{
+			logger.info("cleaning up...");
+			ResponseEntity<VirtualPrivateCloud> responseDelete = vpcService.delete(createdVPC.getId());
+			//Assert.assertEquals(false, responseDelete.isErrors());
+			for (Message message : responseDelete.getMessages()) {
+				logger.warn("Error VPC deletion: [{}] ", message.getMessageText());
+			}
+		}
+	}
+}
---
+++ src/test/java/io/dchq/sdk/core/smoke/testsuite/VPCUpdateServiceTest.java
@@ -0,0 +1,142 @@
+package io.dchq.sdk.core.smoke.testsuite;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+
+import com.dchq.schema.beans.base.Message;
+import com.dchq.schema.beans.base.ResponseEntity;
+import com.dchq.schema.beans.one.base.NameEntityBase;
+import com.dchq.schema.beans.one.security.EntitlementType;
+import com.dchq.schema.beans.one.vpc.VirtualPrivateCloud;
+
+import io.dchq.sdk.core.AbstractServiceTest;
+import io.dchq.sdk.core.ServiceFactory;
+import io.dchq.sdk.core.VPCService;
+
+/**
+ *
+ * @author Jagdeep Jain
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(Parameterized.class)
+public class VPCUpdateServiceTest extends AbstractServiceTest {
+	
+	private VPCService vpcService;
+
+	VirtualPrivateCloud createVPC;
+	VirtualPrivateCloud createdVPC;
+	VirtualPrivateCloud updatedVpc;
+	boolean sussess;
+	long startTime = System.currentTimeMillis();
+	long endTime = startTime + (60 * 60 * 50); // this is for 3 mints
+	
+	public VPCUpdateServiceTest(String vpcName, String providerId, EntitlementType entitlementType, String ipv4Cidr, String description, boolean isprifix, boolean success) {
+		String prifix = RandomStringUtils.randomAlphabetic(3);
+
+		if (vpcName != null && !vpcName.isEmpty() && isprifix) {
+			vpcName = (vpcName + prifix).toLowerCase();
+		}
+		createVPC = new VirtualPrivateCloud();
+		createVPC.setName(vpcName);
+		createVPC.setEntitlementType(entitlementType);
+		createVPC.setIpv4Cidr(ipv4Cidr);
+		NameEntityBase entity = new NameEntityBase();
+		entity.withId(providerId);
+		createVPC.setProvider(entity);
+		createVPC.setDescription(description);
+		this.sussess = success;
+	}
+	@Before
+	public void setUp()
+	{
+		vpcService = ServiceFactory.buildVPCService(rootUrl, cloudadminusername, cloudadminpassword);
+	}
+	@Parameterized.Parameters
+	public static Collection<Object[]> data() throws Exception {
+		return Arrays.asList(new Object[][]{ 
+			// provider id "8a818a105c83f42a015c83fd71240014" Intesar's machine
+			{"testvpc", "2c9180865d312fc4015d314da1ca006a", EntitlementType.OWNER, "10.0.0.0/24", "descriptions test" , true, true},
+		});
+	}
+	
+	@Test
+	public void updateTest()
+	{
+
+		logger.info("Create VPC name[{}] ", createVPC.getName());
+		ResponseEntity<VirtualPrivateCloud> resultResponse = vpcService.create(createVPC);
+		Assert.assertNotNull(resultResponse);
+
+		for (Message msg : resultResponse.getMessages()) {
+			logger.warn("Error [{}]  " + msg.getMessageText());
+		}
+		if (this.sussess) {
+			Assert.assertEquals(false, resultResponse.isErrors());
+			Assert.assertNotNull(resultResponse.getResults());
+
+			if (resultResponse.getResults() != null && !resultResponse.isErrors()) {
+				this.createdVPC = resultResponse.getResults();
+				logger.info("Create VPC Successfull..");
+			}
+			logger.info("VPC state [{}]", createdVPC.getState().name());
+			while(createdVPC.getState().name().equals("PROVISIONING") && (System.currentTimeMillis() < endTime))
+			{
+				try {
+					// wait for some time
+					Thread.sleep(10000);
+					logger.info("VPC state [{}]", createdVPC.getState().name());
+					resultResponse = vpcService.findById(createdVPC.getId());
+					Assert.assertEquals(false, resultResponse.isErrors());
+					Assert.assertNotNull(resultResponse.getResults());
+					this.createdVPC = resultResponse.getResults();
+				} catch (InterruptedException e) {
+					// ignore
+				}
+			}
+			logger.info("VPC state [{}]", createdVPC.getState().name());
+			Assert.assertEquals("LIVE", createdVPC.getState().name());
+			createdVPC.setName(createVPC.getName()+"-updated");
+			ResponseEntity<VirtualPrivateCloud> resultFindResponse = vpcService.update(createdVPC);
+			
+			Assert.assertNotNull(resultFindResponse);
+			Assert.assertEquals(false, resultFindResponse.isErrors());
+			
+			if(resultFindResponse.getResults() != null && !resultFindResponse.isErrors())
+			{
+				this.updatedVpc = resultFindResponse.getResults();
+				logger.info("Create VPC Successful..");
+			}
+			
+			Assert.assertEquals(updatedVpc.getName(), createdVPC.getName());
+
+		} else {
+
+			Assert.assertEquals(true, resultResponse.isErrors());
+			Assert.assertEquals(null, resultResponse.getResults());
+		}
+	
+	}
+	@After
+	public void cleanUp()
+	{
+		if(this.createdVPC !=null)
+		{
+			logger.info("cleaning up...");
+			ResponseEntity<VirtualPrivateCloud> responseDelete = vpcService.delete(createdVPC.getId());
+			for (Message message : responseDelete.getMessages()) {
+				logger.warn("Error vpc deletion: [{}] ", message.getMessageText());
+			}
+		}
+	}
+
+}
