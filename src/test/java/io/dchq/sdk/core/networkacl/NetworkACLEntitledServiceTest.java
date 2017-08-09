2957325c1cb2797f8071d04dce9e522b4e4840c3
skumar1983 <skumar@hypergrid.com>
2017-08-09 11:18:14 +0530

    ignoring n/w acl entitlement. this will work as per vlan entitlement

--- src/test/java/io/dchq/sdk/core/networkacl/NetworkACLEntitledServiceTest.java
+++ src/test/java/io/dchq/sdk/core/networkacl/NetworkACLEntitledServiceTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
@@ -100,8 +101,8 @@ public class NetworkACLEntitledServiceTest extends NetworkACLUtil {
 	}
 
 
-	
-    
+	//Will work as per Vlan Entitlement
+    @Ignore
 	@Test
 	public void testEntitledSearch() {
 		try {
